1b4b8f327df144f4b9efecbe04b3d0070b1c08bc
Shevenkov Alexey <ashevenkov@yandex-team.ru>
2014-04-19 20:23:02 +0400

    started problemB 2013 roundB solution

---
+++ codejam/src/y2008/contest/r1b/ProblemB.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: ashevenkov
+ * Date: 18.04.14
+ * Time: 11:53
+ */
+package y2008.contest.r1b;
+
+/**
+ * @author ashevenkov
+ */
+public class ProblemB {
+}
